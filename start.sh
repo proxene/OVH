aa93bd61771d1de89f0f85f6d2c7f2791d6efb26
Dorian W <idorianw@gmail.com>
2021-10-04 17:55:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ export OVH_SERVER_ID="ns5xxxxx.ip-x-x-x.net"
 export BACKUP_HOST_PREFIX="ftpback-bhs1-x"
 
 yum install nfs-utils rpcbind
-/etc/init.d/rpcbind start
+service rpcbind start
 
 
 sudo mkdir -p /mnt/backups
