991dbbc87f586b045f30372387a00f3cef98c92c
Dorian W <idorianw@gmail.com>
2021-11-24 19:39:29 +0100

    Update ubuntu_nfs.sh

--- ubuntu_nfs.sh
+++ ubuntu_nfs.sh
@@ -5,7 +5,7 @@ set -e
 export OVH_SERVER_ID="ns5xxxxx.ip-x-x-x.net"
 export BACKUP_HOST_PREFIX="ftpback-bhs1-x"
 
-apt-get install nfs-common
+apt-get install nfs-common -y
 
 sudo mkdir -p /mnt/backups
 sudo mount -t nfs "$BACKUP_HOST_PREFIX:/export/ftpbackup/$OVH_SERVER_ID" /mnt/backups
